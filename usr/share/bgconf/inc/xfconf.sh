f27a9d023f5f4ac3f4819c279f7993857c271f05
B Stack <bgstack15@gmail.com>
2017-09-17 08:20:04 -0400

    added xfconf.sh

--- usr/share/bgconf/inc/xfconf.sh
+++ usr/share/bgconf/inc/xfconf.sh
@@ -1,24 +1,23 @@
 #!/bin/sh
-# File: /usr/share/bgconf/inc/dconf.sh
+# File: /usr/share/bgconf/inc/xfconf.sh
 # Author: bgstack15
-# Startdate: 2017-09-17 07:53
-# Title: Script that Loads Settings into Dconf
-# Purpose: To make a single interface for other bgconf scripts to call for loading a dconf file
+# Startdate: 2017-09-17 08:10
+# Title: Script that Loads Settings into Xfconf
+# Purpose: To make a single interface for other bgconf scripts to call for loading an xfconf file
 # History:
 #    2017-06 Main research was done but put in separate bgconf scripts.
 #    2017-09-17 I decided to separate it out to streamline the bgconf scripts themselves.
 # Usage:
-#    In a script, determine that a dconf file exists, then call:
-#       dconf.sh mysettings.dconf
-#    To generate a new dconf file, you can run:
-#       dconf dump / > outfile
+#    In a script, determine that an xfconf file exists, then call:
+#       xfconf.sh mysettings.xfconf
+#    To generate a new xfconf file, you can run:
+#       xfconf-query -l | sed -r -e '/Channels:/d' | while read line; do xfconf-query -lv -c "${line}" | sed -r -e "s/^/${line} /"; done > outfile
 # Reference:
 # Improve:
-#    If I ever need to, pass in thisDE. It is possible other desktop environments use dconf and I will need to grep for 'gnome-shell' or something.
 # Document: Below this line
 
-thisDE=cinnamon
-thisDEconf=dconf
+thisDE=xfce
+thisDEconf=xfconf-query
 infile="${1}"
 
 # get DBUS_SESSION_BUS_ADDRESS of first DE process of this user
@@ -31,7 +30,7 @@ chmod +rx "${tmpfile1}" 2>/dev/null
 . "${tmpfile1}"
 /bin/rm -f "${tmpfile1}" 1>/dev/null 2>&1
 
-# Assume infile exists as a file
+# assume infile exists as a file
 if test -n "$( cat "${infile}" 2>/dev/null )" && test -x "$( which "${thisDEconf}" )";
 then
 
@@ -39,8 +38,16 @@ then
    thisowner="$( stat -c '%U' "${infile}" )"
    thisowneruid="$( stat -c '%u' "${infile}" )"
 
-   # cinnamon custom configuration
-   sudo su - "${thisowner}" -c "cat ${infile} | DISPLAY=${DISPLAY} DBUS_SESSION_BUS_ADDRESS=${DBUS_SESSION_BUS_ADDRESS} ${thisDEconf} load / ;"
+   # xfce custom configuration
+   grep -viE '^\s*((#|;).*)?$' "${infile}" | while read channel attrib value;
+   do
+
+      # display output
+      #printf "channel=%s\tattrib=%s\tvalue\%s\n" "${channel}" "${attrib}" "${value}"
+
+      # make change
+      sudo su - "${thisowner}" -c "DISPLAY=${DISPLAY} DBUS_SESSION_BUS_ADDRESS=${DBUS_SESSION_BUS_ADDRESS} ${thisDEconf} -c ${channel} -p ${attrib} -s ${value}"
+   done
 
 fi
 /bin/rm -f "${tmpfile1}" 2>/dev/null
